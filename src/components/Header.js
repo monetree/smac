7ded12e37ad5db3a06959fa438bbb3a779c1156e
taranjeet <taranjeet@iteratestudio.com>
2023-06-28 14:23:31 +0530

    setup soulmachine react repo

---
+++ src/components/Header.js
@@ -0,0 +1,179 @@
+import React, {Fragment, useEffect} from 'react';
+import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import styled from 'styled-components';
+import { Link, useLocation, useHistory } from 'react-router-dom';
+import {
+  transparentHeader, headerHeight, logoLink,
+} from '../config';
+import Controls from './Controls';
+import {
+  stopSpeaking,
+  setShowTranscript,
+  disconnect,
+  setOutputMute,
+  setMicOn,
+  setCameraOn,
+} from '../store/sm/index';
+import {
+  MicFill,
+  MicMuteFill,
+} from 'react-bootstrap-icons';
+import { primaryAccent } from '../globalStyle';
+
+const iconSize = 24;
+
+function Header({
+  className,
+}) {
+  const dispatch = useDispatch();
+  const { pathname } = useLocation();
+  const { connected, loading, micOn, highlightMic } = useSelector(({ sm }) => ({ ...sm }));
+
+
+  const history = useHistory();
+  
+  const getUserInfo = () => {
+    let userInfo = localStorage.getItem("userInfo");
+    if(!userInfo){
+      return null;
+    }
+    userInfo = JSON.parse(userInfo);
+    if(!userInfo?.name){
+      return null;
+    }
+    return userInfo;
+  }
+
+  useEffect(()=> {
+    let isLoggedInUser = getUserInfo();
+    if(!isLoggedInUser){
+      history.push('/');
+    }
+  }, []);
+  
+
+  return (
+    <Fragment>
+      <div className={`${className}`}>
+        <div className="container">
+          <div className="row">
+            <div className="d-flex align-items-center justify-content-between">
+              <div>
+                {/* left align */}
+                {/* <Link to={logoLink}  className={`${pathname === '/loading' ? 'logo' : 'd-none'}`}>
+                  Polyverse
+                </Link> */}
+                <aside id="menu" className={"user-menu"}>
+                    <div className="menu-child">
+                      <img
+                        id="profile-pic"
+                        src={getUserInfo().picture}
+                        width={55}
+                        height={55}
+                      />
+                      <div>
+                        <p className={`username ${pathname === '/loading' ? "black" : ""}`}>
+                          <b>Welcome </b>
+                          {getUserInfo().name}
+                        </p>
+                      
+                      </div>
+                    </div>
+                  </aside>
+              </div>
+              <div>
+                {/* middle align */}
+              </div>
+              <div>
+                {/* right align */}
+                <div className={`${connected && !loading && pathname === '/video' ? '' : 'd-none'}`}>
+                  <Controls />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {connected &&
+        <div className={"action-wrapper action-btns"}>
+            {!micOn && (
+              <button
+                onClick={() => dispatch(setMicOn({ micOn: !micOn }))}
+                id="un-mute-icon"
+                color="primary"
+                className=" mat-focus-indicator microphone mat-fab mat-button-base mat-primary"
+              >
+                <span className="mat-button-wrapper">
+                <MicMuteFill size={iconSize} style={{ border: highlightMic ? 'red 2px solid' : '' }} />
+                </span>
+                <span
+                  matripple=""
+                  className="mat-ripple mat-button-ripple mat-button-ripple-round"
+                ></span>
+                <span className="mat-button-focus-overlay"></span>
+              </button>
+            )}
+            {micOn && (
+              <button
+                onClick={() => dispatch(setMicOn({ micOn: !micOn }))}
+                id="mute-icon"
+                color="primary"
+                className="un-mute mat-focus-indicator microphone mat-fab mat-button-base mat-primary"
+              >
+                <span className="mat-button-wrapper">
+                <MicFill size={iconSize} color={primaryAccent} style={{ border: highlightMic ? 'red 2px solid' : '' }} />
+                </span>
+                <span
+                  matripple=""
+                  className="mat-ripple mat-button-ripple mat-button-ripple-round"
+                ></span>
+                <span className="mat-button-focus-overlay"></span>
+              </button>
+            )}
+
+            <div
+              id="speech-indicator"
+              className={`speech-indicator ${!micOn ? "mute" : ""}`}
+            >
+              <span className="speech-indicator-span"></span>
+              <span className="speech-indicator-span"></span>
+              <span className="speech-indicator-span"></span>
+              <span className="speech-indicator-span"></span>
+              <span className="speech-indicator-span"></span>
+              <span className="speech-indicator-span"></span>
+            </div>
+        </div>
+      }
+    </Fragment>
+  );
+}
+Header.propTypes = {
+  className: PropTypes.string.isRequired,
+};
+
+export default styled(Header)`
+  position: relative;
+  z-index: 20;
+  top: 0;
+  width: 100%;
+  background-color: ${transparentHeader ? 'none' : '#FFFFFF'};
+
+  &>.row {
+    height: ${headerHeight};
+  }
+  .logo {
+    margin-top: 20px;
+
+    display: block;
+    width: 150px;
+    font-weight: 600;
+    font-size: 1.5rem;
+    line-height: 2rem;
+
+    // Medium devices (tablets, 768px and up)
+    @media (min-width: 768px) {
+      height: calc(0.8 * ${headerHeight});
+   }
+  }
+`;
---
+++ src/routes/Landing.js
@@ -0,0 +1,235 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import Color from 'color';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { CameraVideoFill, MicFill } from 'react-bootstrap-icons';
+import breakpoints from '../utils/breakpoints';
+import Header from '../components/Header';
+import { landingBackgroundImage, landingBackgroundColor } from '../config';
+import { setRequestedMediaPerms } from '../store/sm';
+import micFill from '../img/mic-fill.svg';
+import videoFill from '../img/camera-video-fill.svg';
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+import GoogleButton from "react-google-button";
+import { useHistory } from 'react-router-dom';
+import './index.css';
+
+function Landing({ className }) {
+  const history = useHistory();
+  // const { mic, camera } = useSelector(({ sm }) => sm.requestedMediaPerms);
+  // const dispatch = useDispatch();
+
+  const [user, setUser] = useState([]);
+  const emails = [
+    "mailbox@phanig.com",
+    "soubhagyakumar666@gmail.com",
+    "pinnamanenivvs@gmail.com",
+    "dprabhakar@webprodigiesinc.com",
+    "taranjeetsingh3326@gmail.com",
+    "claire.shiying@gmail.com",
+    "ashishmohanty000@gmail.com",
+    "manmohanalla@gmail.com",
+    "genesaigal@gmail.com",
+    "erilineresorts@gmail.com",
+    "sameera1892@gmail.com",
+    "sanjeev@vscgroup.co",
+    "vpsps@yahaoo.com",
+    "vpspublicschool@gmail.com",
+    "ikonsway@gmail.com",
+    "bablusb143@gmail.com",
+    "sfwhyd@gmail.com",
+    "krudra@gmail.com",
+    "mail4lucky@gmail.com",
+    "Kchawalla79@gmail.com",
+    "pasupuleti9@gmail.com",
+    "agrrohit11@gmail.com",
+    "kunapareddypadmasree@gmail.com",
+    "shaikhzameer99@gmail.com",
+    "saunakasae21@gmail.com",
+    "apernavolluru14@gmail.com",
+    "turukrish@gmail.com",
+    "skillioma.work@gmail.com",
+    "sbharat4u1985@gmail.com",
+    "chaitoos82@gmail.com",
+    "pradeepsutodiya1@gmail.com",
+    "jainashoakkumar@gmail.com",
+  ];
+
+  const getUserInfo = () => {
+    let userInfo = localStorage.getItem("userInfo");
+    if(!userInfo){
+      return null;
+    }
+    userInfo = JSON.parse(userInfo);
+    if(!userInfo?.name){
+      return null;
+    }
+    return userInfo;
+  }
+
+  useEffect(()=> {
+    let isLoggedInUser = getUserInfo();
+    if(isLoggedInUser){
+      history.push('/loading');
+    }
+  }, []);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log("Login Failed:", error),
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          if (!emails.includes(res.data.email)) {
+            alert("You don't have access to this page .. ");
+            return;
+          } else {
+            console.log("***google Info**", res.data);
+            localStorage.setItem("userInfo", JSON.stringify(res.data));
+            history.push('/loading');
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+
+  return (
+    <div className={className}>
+      {/* <div className="landing-wrapper"> */}
+        <div id="content" className="login-container">
+          <div className="login-container-block card-">
+            <div className="">
+              <h1 className="login-title"> Polyverse</h1>
+              <h3 className="login-title"> Sign in</h3>
+              {/* <h4 className="login-title">to continue to Alie Polyverse</h4> */}
+            </div>
+            <div className="btn-position">
+              <div className="space-y-3 text-center ">
+                {/* <button onClick={() => login()}>Sign in with Google 🚀 </button> */}
+                <GoogleButton onClick={() => login()} />
+              </div>
+            </div>
+          </div>
+        </div>
+       
+      {/* </div> */}
+    </div>
+  );
+}
+
+Landing.propTypes = {
+  className: PropTypes.string.isRequired,
+};
+
+export default styled(Landing)`
+  .landing-wrapper {
+    min-height: 100vh;
+
+    background: ${landingBackgroundImage ? `url(${landingBackgroundImage})` : ''} ${landingBackgroundColor ? `${landingBackgroundColor};` : ''};
+    background-size: auto 60%;
+    background-repeat: no-repeat;
+    background-position: bottom center;
+
+    @media (min-width: ${breakpoints.lg}px) {
+      background-size: 60% auto;
+      background-position: right bottom;
+    }
+  }
+  .landing-container {
+    padding-top: 1rem;
+    display: flex;
+
+    &>div {
+      background-color: ${Color(landingBackgroundColor).alpha(0.5)};
+      backdrop-filter: blur(10px);
+      border: 1px solid rgba(0,0,0,0.1);
+      padding: 1rem;
+      border-radius: 5px;
+
+      @media (min-width: ${breakpoints.lg}px) {
+        border: none;
+      }
+    }
+  }
+  .form-switch .form-check-input {
+    min-width: 7rem;
+    height: 3rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+
+    &.mic-switch::before, &.mic-switch.status-checked::after {
+        background-image: url(${micFill});
+    }
+    &.video-switch::before, &.video-switch.status-checked::after {
+        background-image: url(${videoFill});
+    }
+    &.mic-switch.status-checked::before, &.video-switch.status-checked::before {
+      background-image: none;
+    }
+
+    &.status-unchecked {
+      &::after {
+        content: 'OFF';
+        color: #000;
+        margin-right: 18%;
+      }
+      &::before {
+        background-size: 60%;
+        background-repeat: no-repeat;
+        background-color: rgb(220, 220, 220);
+        background-position: 45% center;
+        content: '';
+        display: block;
+
+        border-radius: 50%;
+
+        height: 80%;
+        margin-left: 5%;
+        aspect-ratio: 1;
+        float: right;
+      }
+    }
+
+    &.status-checked {
+      &::before {
+        content: 'ON';
+        color: #FFF;
+        margin-left: 22%;
+      }
+
+      &::after {
+        background-size: 60%;
+        background-repeat: no-repeat;
+        background-color: #FFF;
+        background-position: 55% center;
+        content: '';
+        display: block;
+
+        border-radius: 50%;
+
+        height: 80%;
+        margin-right: 5%;
+        aspect-ratio: 1;
+        float: right;
+      }
+    }
+  }
+
+`;
