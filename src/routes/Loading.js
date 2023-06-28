deeb17ff898c9a0d9f1a3f2c998a7afbe1440762
taranjeet <taranjeet@iteratestudio.com>
2023-06-28 22:07:04 +0530

    ulpdated loader screen

--- src/routes/Loading.js
+++ src/routes/Loading.js
@@ -53,112 +53,24 @@ function Loading({
   const pages = [
     <div>
       <div className="row justify-content-center">
-        <div className="tutorial-icon mb-2">
-          <MicFill size={iconSize} />
-        </div>
-      </div>
-      <div className="row">
-        <div className="d-flex align-items-center justify-content-between">
-          <button className="btn-unstyled" type="button" style={{ opacity: 0, width: '44px' }}>
-            {' '}
-          </button>
-          <h4>
-            Before you begin.
-          </h4>
-          <button className="btn-unstyled" type="button" onClick={() => setPage(page + 1)}>
-            <ArrowRightCircleFill size={32} />
-          </button>
-        </div>
-        <div className="mt-0 mb-2">
-          {
-            // show different modal if mic is off or if mic perms are denied
-            requestedMediaPerms.mic === true && requestedMediaPerms.micDenied === false
-              ? (
-                <div>
-                  <p>
-                    The Digital Person works best in a quiet environment, when you&apos;re close to
-                    your microphone, and your camera is on.
-                  </p>
-                  <p>
-                    Speak clearly, and in short responses.
-                  </p>
-                </div>
-              )
-              : (
-                <div>
-                  <p>
-                    The Digital Person works best with your microphone on. Enable your microphone
-                    at any point during the experience by clicking the microphone icon & allowing
-                    the permissions.
-                  </p>
-                  <p>
-                    Remember to speak clearly, and in short responses.
-                  </p>
-                </div>
-              )
-          }
-        </div>
-      </div>
-    </div>,
-    <div>
-      <div className="row justify-content-center">
-        <div className="tutorial-icon mb-2">
-          <div className="fs-4 fw-bold mt-2">
-            &ldquo;hi, how are you?&rdquo;
+        {/* <div className="tutorial-icon mb-2"> */}
+        <div className="loader ">
+          <div className="spinner-border" role="status">
+            <span className="sr-only"></span>
           </div>
         </div>
+        {/* </div> */}
       </div>
-      <div className="row">
-        <div className="d-flex align-items-center justify-content-between">
-          <button className="btn-unstyled" type="button" onClick={() => setPage(page - 1)}>
-            <ArrowLeftCircleFill size={32} />
-          </button>
-          <h4>
-            What you do.
-          </h4>
-          <button className="btn-unstyled" type="button" onClick={() => setPage(page + 1)}>
-            <ArrowRightCircleFill size={32} />
-          </button>
-        </div>
-        <div className="mt-0 mb-2">
-          Cory will listen to whatever you say.
-          Other options, like typing or choosing your responses, are also available.
-        </div>
-      </div>
-    </div>,
-    <div>
-      <div className="row justify-content-center">
-        <div className="tutorial-icon tutorial-icon-dp mb-2" />
-      </div>
-      <div className="row">
-        <div className="d-flex align-items-center justify-content-between">
-          <button className="btn-unstyled" type="button" onClick={() => setPage(page - 1)}>
-            <ArrowLeftCircleFill size={32} />
-          </button>
-          <h4>
-            What you can talk about.
-          </h4>
-          <button className="btn-unstyled" type="button" style={{ opacity: 0, width: '44px' }}>
-            {' '}
-          </button>
-        </div>
-        <div className="mt-0 mb-2">
-          You can explore the features of my UI, see examples of the different
-          supporting content I can show, or hear about the latest updates to
-          the UI template that I&apos;m using.
-        </div>
-      </div>
-    </div>,
+    </div>
   ];
 
   const [skip, setSkip] = useState(false);
-  const redirectToVideoOnConnect = () => {
-    setSkip(true);
-  };
+ 
   const history = useHistory();
+
   useEffect(() => {
-    if (skip === true && connected === true) history.push('/video');
-  }, [connected, skip]);
+    if ( connected === true) history.push('/video');
+  }, [connected]);
 
   
 
@@ -171,75 +83,7 @@ function Loading({
             <div className="row">
               {pages[page]}
             </div>
-            <div className="row justify-content-center">
-              <div>
-                {
-                  page < pages.length - 1
-                    ? (
-                      <button
-                        className="btn primary-accent m-2"
-                        type="button"
-                        onClick={() => setPage(page + 1)}
-                        style={{ backgroundColor: '#3C3C3C', border: '2px solid #3C3C3C' }}
-                      >
-                        Next
-                      </button>
-                    )
-                    : null
-                }
-              </div>
-            </div>
-            <div className="row">
-              <div>
-                <button
-                  className={`${connected || page >= pages.length - 1 ? 'btn btn-dark connected-button' : 'btn-unstyled unconnected-button'} m-2`}
-                  type="button"
-                  disabled={skip}
-                  onClick={redirectToVideoOnConnect}
-                >
-                  { connected || page >= pages.length - 1 ? 'Chat Now' : 'S K I P' }
-                  { !connected && skip
-                    ? (
-                      <div className="ms-1 spinner-border spinner-border-sm text-secondary" role="status">
-                        <span className="visually-hidden">Loading...</span>
-                      </div>
-                    )
-                    : null }
-                </button>
-              </div>
-            </div>
-            <div className="row justify-content-center">
-              <div>
-                {/* eslint-disable-next-line react/no-array-index-key */}
-                {pages.map((_, i) => (<div key={`${i}-${i === page}`} className="d-inline-block p-1">{i === page ? <div className="closed-dot" /> : <div className="open-dot" />}</div>))}
-              </div>
-            </div>
-            {
-              percentageLoaded < 100
-                ? (
-                  <div>
-                    <div className="progress mt-1">
-                      <div
-                        className="progress-bar"
-                        role="progressbar"
-                        style={{ width: `${percentageLoaded}%` }}
-                        aria-label={`${stateName} (${currentStep} out of ${totalSteps - 1})`}
-                        aria-valuenow={percentageLoaded}
-                        aria-valuemin="0"
-                        aria-valuemax="100"
-                      />
-                    </div>
-                    { stateName !== ''
-                      ? (
-                        <pre>
-                          {`${stateName} (${currentStep} out of ${totalSteps - 1} steps)`}
-                        </pre>
-                      )
-                      : null}
-                  </div>
-                )
-                : null
-            }
+          
           </div>
         </div>
       </div>
