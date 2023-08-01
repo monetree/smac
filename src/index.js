225ce017dee7af91c76f9ceef351f53b705a900f
soubhagya <soubhagyakumar666@gmail.com>
2023-08-01 20:15:35 +0530

    added datadog

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { datadogRum } from "@datadog/browser-rum";
 import ReactTooltip from "react-tooltip";
 import Router from "./Router";
 import store from "./store";
 import reportWebVitals from "./reportWebVitals";
 import GlobalStyle from "./globalStyle";
+
+datadogRum.init({
+  applicationId: "67577500-d823-467a-bd4e-db51e0434f2f",
+  clientToken: "pub8733e8acc71d1eadad12e5af290ff7e7",
+  site: "us5.datadoghq.com",
+  service: "avatarx",
+  env: "prod",
+  sessionSampleRate: 100,
+  premiumSampleRate: 100,
+  trackUserInteractions: true,
+  defaultPrivacyLevel: "mask-user-input",
+});
+
+datadogRum.startSessionReplayRecording();
 
 ReactDOM.render(
   <React.Fragment>
